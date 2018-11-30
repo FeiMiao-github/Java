465dba8344f9dd66d8816affa27e44e785de3a5d
FeiMiao-github <3138731516@qq.com>
2018-11-30 23:35:45 +0800

    清除辣鸡信息

--- javaDemo/demo8.java
+++ javaDemo/demo8.java
@@ -1,3 +1,21 @@
 // 构造器中调用构造器
 
-public class 
+public class demo8 {
+    private String words = "this is a statement.";
+
+    demo8() {
+        System.out.println(this.words + "this.words");
+        this.words = "this statement has been changed!";
+        System.out.println("demo8() run!");
+    }
+
+    demo8(String w) {
+        this();
+        System.out.println("调用构造器 demo8(String w)！ w = " + w);
+
+    }
+    
+    public static void main(String[] args) {
+        new demo8("hello world");
+    }
+}
