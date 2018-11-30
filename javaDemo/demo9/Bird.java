fee63d33449399997ab1bea28865363f4531c970
FeiMiao-github <3138731516@qq.com>
2018-12-01 00:12:06 +0800

    a funny example

---
+++ javaDemo/demo9/Bird.java
@@ -0,0 +1,10 @@
+// 疯狂java讲义中一个好玩的例子
+// 重写父类方法
+
+public class Bird {
+    // 假设这是一个bird 类
+
+    public void fly() {
+        System.out.println("i am a bird and i can fly!");
+    }
+}
---
+++ javaDemo/demo9/Ostrich.java
@@ -0,0 +1,6 @@
+public class Ostrich extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("i am a bird, but i can only run on the ground!");
+    }
+}
