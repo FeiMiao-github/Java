2be847eede7e22b5f919b12e6f5d08dd9ed8daf4
FeiMiao-github <3138731516@qq.com>
2018-12-04 23:52:28 +0800

    demo18: java类的新成员，初始化代码块

---
+++ javaDemo/demo18.java
@@ -0,0 +1,58 @@
+public class demo18 {
+    public static void main(String[] args) {
+        /**1
+         * Demo.sayS();
+         * 输出结果： 
+         * 长在红旗下的好少年！=> 静态代码块
+         * hello, 树先生！ => 静态方法
+         **/
+
+        /** 2
+         * String s = Demo.stringS;
+         * System.out.println(s);
+         * 输出结果：
+         * 长在红旗下的好少年！
+         * 忠犬八公
+         **/
+        
+        /**3
+         * new Demo();
+         * 长在红旗下的好少年！
+         * 构造
+         */
+
+        
+        /**4
+         * new Demo().say();
+         * 长在红旗下的好少年！
+         * 忠犬八公
+         * 构造
+         * 这个杀手不太冷
+         */
+    }
+}
+
+class Demo {
+    String string = "皮皮虾号";
+
+    static String stringS = "忠犬八公";
+    private int num = 1;
+
+
+    public static void sayS() {
+        System.out.println("hello, 树先生！");
+    }
+
+    public void say() {
+        System.out.println("这个杀手不太冷");
+    }
+
+    public Demo() {
+        System.out.println("构造");
+    }
+
+    static {
+        System.out.println("长在红旗下的好少年！");
+        System.out.println(stringS);
+    }
+}
