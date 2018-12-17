ea27783113132032a2875bea4f2a7d7d23b0eb42
FeiMiao-github <3138731516@qq.com>
2018-12-17 23:25:40 +0800

    接口的功能

---
+++ javaDemo/demo36.java
@@ -0,0 +1,48 @@
+// 使用接口
+/**
+ * demo36
+ */
+public class demo36 implements Product{
+    public static void main(String[] args) {
+        demo36 demo = new demo36();
+        demo.test();
+    }
+    
+    public void test() {
+        // TODO: 测试代码
+    }
+
+    private String[] printDate = new String[InnerInterfaceDemo36.MAX_CACHE_LINE];
+    private int dateNum = 0;
+
+    public void out() {
+        while (dateNum > 0) {
+            System.out.println("打印机打印 --- " + printDate[0]);
+            System.arraycopy(printDate, 1, printDate, 0, --dateNum);
+        }
+    }
+
+    public void getDate(String msg) {
+        if (dateNum >= InnerInterfaceDemo36.MAX_CACHE_LINE) {
+            System.out.println("输出队列已满，添加错误");
+        } else {
+            printDate[dateNum++] = msg;
+        }
+    }
+
+    @Override
+    public int getProduceTime() {
+        return 45;
+    }
+    /**
+     * InnerInterfaceDemo36
+     */
+    public interface InnerInterfaceDemo36 {
+        int MAX_CACHE_LINE = 3;
+    }
+}
+
+interface Product {
+    int getProduceTime();
+}
+
