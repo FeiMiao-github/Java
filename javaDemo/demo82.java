ece46fbb513842ac22155e6157c45052392f579c
FeiMiao-github <3138731516@qq.com>
2019-01-03 00:00:24 +0800

    InputStreamReader BufferedReader

---
+++ javaDemo/demo82.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class demo82 {
+	public static void main(String[] args) {
+		new demo82().test();
+	}
+	
+	public void test() {
+		String s = null;
+		try(
+				InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+				BufferedReader bufReader = new BufferedReader(inputStreamReader)){
+			try {
+				while ((s = bufReader.readLine()) != null) {
+					if (s.equals("exit") || s.equals("quit")) {
+						System.exit(0);
+					}
+					System.out.println("[ReadLine]" + s);
+				}
+			} finally {
+				inputStreamReader.close();
+				bufReader.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
