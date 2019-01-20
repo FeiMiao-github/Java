484084c90fe79543189b6bdcad9671ac41b9a3df
FeiMiao-github <3138731516@qq.com>
2019-01-20 21:27:22 +0800

    project

---
+++ javaDemo2/demo/src/I18N/demo96.java
@@ -0,0 +1,14 @@
+package I18N;
+
+/* 使用NumberFormatt 格式化数字*/
+public class demo96 {
+	public static void main(String[] args) {
+		demo96 demo = new demo96();
+		
+		demo.test();
+	}
+	
+	void test() {
+		
+	}
+}
